a5caa47b2d0b87b24afae89472d94efe2b456bd3
sjljrvis <sjlchougule@gmail.com>
2017-12-06 02:24:38 +0530

    added nginx command

--- app/helper/functions.js
+++ app/helper/functions.js
@@ -2,6 +2,7 @@ var exec = require('child_process').exec;
 
 export const execshell = (command, callback) => {
   exec(command, (error, stdout, stderr) => {
+    conole.log("I am here", stderr , stdout)
     callback(error,stdout);
   });
 }
