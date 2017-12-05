a706253989340f7ccd65411f491cec33f5222eae
sjljrvis <sjlchougule@gmail.com>
2017-12-06 02:27:46 +0530

    fixed in uer contrller

--- app/helper/functions.js
+++ app/helper/functions.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec;
 
 export const execshell = (command, callback) => {
   exec(command, (error, stdout, stderr) => {
-    conole.log("I am here", stderr , stdout)
+    console.log("I am here", stderr , stdout)
     callback(error,stdout);
   });
 }
