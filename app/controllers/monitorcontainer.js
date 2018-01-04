356656bcc79510befa7d47fbb95feb315464c82c
sjjrvis <sjlchougule@gmail.com>
2018-01-05 02:20:05 +0530

    host based routing handled

--- app/controllers/monitorcontainer.js
+++ app/controllers/monitorcontainer.js
@@ -1,30 +1,30 @@
 var exec = require('child_process').exec;
 
 module.exports.monitorContainer = (req, res) => {
-  let containerName = req.query.containerName;
-  execute('docker inspect ' + req.query.containerName, (result) => {
-    result = JSON.parse(result);
-    if (result.length > 0) {
-      res.status(200).json(result);
-    }
-    else {
-      let _result = [{
-        State: { Pid: 0 },
-        HostConfig: { ShmSize: 0 },
-        NetworkSettings: { IPAddress: 0, MacAddress: 0 }
-      }
-      ]
-      res.status(200).json(_result);
-    }
-  })
+	let containerName = req.query.containerName;
+	execute('docker inspect ' + req.query.containerName, (result) => {
+		result = JSON.parse(result);
+		if (result.length > 0) {
+			res.status(200).json(result);
+		}
+		else {
+			let _result = [{
+				State: { Pid: 0 },
+				HostConfig: { ShmSize: 0 },
+				NetworkSettings: { IPAddress: 0, MacAddress: 0 }
+			}
+			]
+			res.status(200).json(_result);
+		}
+	})
 }
 
 module.exports.executeCommand = (req, res) => {
-  let containerName = req.query.containerName;
-  let command = req.body.command
-  execute('docker run ' + req.query.containerName + " " + command, (result) => {
-    res.status(200).send(result);
-  })
+	let containerName = req.query.containerName;
+	let command = req.body.command
+	execute('docker run ' + req.query.containerName + " " + command, (result) => {
+		res.status(200).send(result);
+	})
 
 }
 
@@ -32,12 +32,18 @@ module.exports.viewGitCommits = (req, res) => {
 
 }
 module.exports.fetchLogs = (req, res) => {
-  execute('docker logs sejal_testingapp_stunning-wing', (result) => {
-    res.send(result);
-  })
+	execute('docker logs sejal_testingapp_stunning-wing', (result) => {
+		res.send(result);
+	})
+}
+
+module.exports.reloadNginx = (req, res) => {
+	execute('sudo service nginx reload', (result) => {
+		res.send(result);
+	})
 }
 var execute = (command, callback) => {
-  exec(command, (error, stdout, stderr) => {
-    callback(stdout);
-  });
+	exec(command, (error, stdout, stderr) => {
+		callback(stdout);
+	});
 };
--- app/routes/monitorcontainer.js
+++ app/routes/monitorcontainer.js
@@ -4,4 +4,5 @@ export const monitorcontainerRoute = (app) => {
 			app.get('/viewgitcommits', require(__base +'/app/controllers/monitorcontainer.js').viewGitCommits);
 			app.get('/fetchlogs', require(__base +'/app/controllers/monitorcontainer.js').fetchLogs);
 			app.post('/executecommand', require(__base +'/app/controllers/monitorcontainer.js').executeCommand);
+			app.get('/reloadnginx' , require(__base +'/app/controllers/monitorcontainer.js').reloadNginx)
 	};
