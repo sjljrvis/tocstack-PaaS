a07fc51f16f34bd8595c1575d1557e57974c9dae
sjjrvis <sjlchougule@gmail.com>
2018-01-18 01:18:29 +0530

    exec commant for cli fixed

--- app/controllers/monitorcontainer.js
+++ app/controllers/monitorcontainer.js
@@ -34,7 +34,7 @@ module.exports.monitorContainer = (req, res) => {
 module.exports.executeCommand = (req, res) => {
 	let containerName = req.query.containerName;
 	let command = req.body.command
-	execute('docker run ' + req.query.containerName + " " + command, (result) => {
+	execute('docker exec ' + req.query.containerName + " " + command, (result) => {
 		res.status(200).send(result);
 	})
 
